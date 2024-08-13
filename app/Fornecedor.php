24bb2111508e481b7b67bfc8774f2f0fbb0b93f9
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-13 14:59:37 +0930

    Adding softDelete migration to forncedores.

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,11 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    use SoftDeletes;
     // Ao usar var $table o eloquent sobrescreve o nome padrão do model + s
     protected $table = 'fornecedores';
 
     protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
+
