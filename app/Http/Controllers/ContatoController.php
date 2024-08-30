9d9633fab092300b782ed80a5729f2986fbf002a
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-30 19:02:56 +0930

    Ädding require as a simple validation form.

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,28 +8,18 @@ use App\SiteContato;
 class ContatoController extends Controller
 {
     function contato(Request $request) {
-        // echo '<pre>';
-        // print_r($request->all());
-        // echo '</pre>';
-
-        $contato = new SiteContato();
-
-        // Method #1 to create an obj
-        // $contato->nome = $request->input('nome');
-        // $contato->telefone = $request->input('telefone');
-        // $contato->email = $request->input('email');
-        // $contato->motivo_contato = $request->input('motivo_contato');
-        // $contato->mensagem = $request->input('mensagem');
-        
-        // Method #2 to create an obj
-        // $contato->create($request->all());
-        
-        // Method #3 to create an obj
-        $contato->fill($request->all());
-        
-        $contato->save();
-        // print_r($contato->getAttributes());
 
         return view('site.contato', ['titulo' => 'Contato (teste)']);
+    }
+
+    function salvar(Request $request) {
+        $request->validate([
+            'nome' => 'required',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required'
+        ]);
+        // SiteContato::create($request->all());
     }
 }
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,17 +1,23 @@
 {{ $slot }}
 <form action={{ route('site.contato') }} method="post">
     @csrf
-    <input name="nome" type="text" placeholder="Nome" class="{{ $classe }}" value="{{ old('nome') }}" required>
-    <input name="telefone" type="text" placeholder="Telefone" class="{{ $classe }}" value="{{ old('telefone') }}" required>
-    <input name="email" type="email" placeholder="E-mail" class="{{ $classe }}" value="{{ old('email') }}" required>
-    <select name="motivo_contato" class="{{ $classe }}" required>
+    <input name="nome" type="text" placeholder="Nome" class="{{ $classe }}" value="{{ old('nome') }}">
+    <input name="telefone" type="text" placeholder="Telefone" class="{{ $classe }}" value="{{ old('telefone') }}">
+    <input name="email" type="email" placeholder="E-mail" class="{{ $classe }}" value="{{ old('email') }}">
+    <select name="motivo_contato" class="{{ $classe }}">
         <option value="" disabled {{ old('motivo_contato') === null ? 'selected' : '' }}>Qual o motivo do contato?</option>
         <option value="1" {{ old('motivo_contato') == 1 ? 'selected' : '' }}>Dúvida</option>
         <option value="2" {{ old('motivo_contato') == 2 ? 'selected' : '' }}>Elogio</option>
         <option value="3" {{ old('motivo_contato') == 3 ? 'selected' : '' }}>Reclamação</option>
     </select>
-    <textarea name="mensagem" placeholder="Preencha aqui a sua mensagem" class="{{ $classe }}" required>{{ old('mensagem') }}</textarea>
+    <textarea name="mensagem" placeholder="Preencha aqui a sua mensagem" class="{{ $classe }}">{{ old('mensagem') }}</textarea>
 
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
+
+<div style="position:absolute; top:0px; left:0px; width:100%; background:red">
+    <pre>
+        {{ print_r($errors) }}
+    </pre>
+</div>
