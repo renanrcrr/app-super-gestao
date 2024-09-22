a1703bd6c686d6149f487bcc99a9df0688821ce9
renanrcrr <renanrcrr.developer@gmail.com>
2024-09-22 20:59:12 +0930

    Persisting data and redirecting a route.

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,31 +1,28 @@
 {{ $slot }}
 <form action={{ route('site.contato') }} method="post">
     @csrf
-    <input name="nome" type="text" placeholder="Nome" class="{{ $classe }}" value="{{ old('nome') }}">
-    <input name="telefone" type="text" placeholder="Telefone" class="{{ $classe }}" value="{{ old('telefone') }}">
-    <input name="email" type="email" placeholder="E-mail" class="{{ $classe }}" value="{{ old('email') }}">
-    <select name="motivo_contato" class="{{ $classe }}">
-        <option value="" disabled {{ old('motivo_contato') === null ? 'selected' : '' }}>Qual o motivo do contato?</option>
+    <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    <br>
+    <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    <br>
+    <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    <br>
 
-        @foreach ($motivo_contatos as $key => $motivo_contato)
-            <option value="{{$motivo_contato->id}}" {{ old('motivo_contato') == $motivo_contato->id ? 'selected' : '' }}>{{$motivo_contato->motivo_contato}}</option>
+    <select name="motivo_contatos_id" class="{{ $classe }}">
+        <option value="">Qual o motivo do contato?</option>
+
+        @foreach($motivo_contatos as $key => $motivo_contato)
+            <option value="{{$motivo_contato->id}}" {{ old('motivo_contatos_id') == $motivo_contato->id ? 'selected' : '' }}>{{$motivo_contato->motivo_contato}}</option>
         @endforeach
     </select>
-    
-    <textarea name="mensagem" placeholder="Preencha aqui a sua mensagem" class="{{ $classe }}">
-        @if (old('mensagem') != '')
-            {{ old('mensagem') }}
-        @else
-            Preencha aqui sua mensagem
-        @endif
-    </textarea>
-
+    <br>
+    <textarea name="mensagem" class="{{ $classe }}">{{ (old('mensagem') != '') ? old('mensagem') : 'Preencha aqui a sua mensagem' }}</textarea>
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
-</form>  
+</form>
 
-<div style="position:absolute; top:0px; left:0px; width:100%; background:red">
+<div style="position:absolute; top:0px; width:100%; background:red">
     <pre>
-        {{ print_r($errors) }}
+    {{ print_r($errors) }}
     </pre>
 </div>
