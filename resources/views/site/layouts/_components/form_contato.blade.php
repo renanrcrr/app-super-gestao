41e9e008e8786ef3b5f574f1b18606457d846ad8
renanrcrr <renanrcrr.developer@gmail.com>
2024-09-23 00:03:40 +0930

    Customizing validation errors.

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -21,8 +21,10 @@
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
 
-<div style="position:absolute; top:0px; width:100%; background:red">
-    <pre>
-    {{ print_r($errors) }}
-    </pre>
-</div>
+@if ($errors->any())
+    <div style="position:absolute; top:0px; width:100%; background:red">
+        @foreach ($errors->all() as $error)
+            {{ $error }} <br/>
+        @endforeach
+    </div>
+@endif
