702706f790ea7be690761b1bbdb6e856d4a8464c
renanrcrr <renanrcrr.developer@gmail.com>
2024-09-30 08:08:47 +0930

    Created model LogAccess and its migration.

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\LogAcesso;
 
 class LogAcessoMiddleware
 {
@@ -16,6 +17,11 @@ class LogAcessoMiddleware
     public function handle($request, Closure $next)
     {
         // return $next($request);
+        $ip = $request->server->get('REMOTE_ADDR');
+        $rota = $request->getRequestUri();
+
+        LogAcesso::create(['log' => "IP $ip requisitou a rora $rota"]);
+
         return Response('Hey chegou no middleware, mas não adiante!');
     }
 }
