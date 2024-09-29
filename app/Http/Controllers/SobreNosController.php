7fe3bc8678a1b57e34386a35a22c4c30b8958af3
renanrcrr <renanrcrr.developer@gmail.com>
2024-09-30 08:21:54 +0930

    Adding middlewares in the controllers construct.

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Middleware\LogAcessoMiddleware;
 
 class SobreNosController extends Controller
 {
-    function sobreNos() {
+    public function __construct(){
+        $this->middleware(LogAcessoMiddleware::class);
+    }
+
+    public function sobreNos() {
         return view('site.sobre-nos');
     }
 }
