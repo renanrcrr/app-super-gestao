3c9750a0050d778171f94eed24edfc84914e2e35
renanrcrr <renanrcrr.developer@gmail.com>
2024-11-10 10:12:11 +1030

    Passing parameters to middleware.

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -13,8 +13,21 @@ class AutenticacaoMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $metodo_autenticacao, $perfil)
     {
+        echo $metodo_autenticacao.' - '.$perfil.'<br>';
+
+        if($metodo_autenticacao == 'padrao'){
+            echo 'Verificar o usuário e senha no banco de dados'.$perfil.'<br>';
+        }
+        if($metodo_autenticacao == 'ldap'){
+            echo 'Verificar o usuário e senha no AD'.$perfil.'<br>';
+        }
+        if($perfil == 'visitante') {
+            echo 'Exibir apenas alguns visitantes';
+        } else {
+            echo 'Carregar o perfil no banco de dados';
+        }
         if(false){
             return $next($request);
         } else {
