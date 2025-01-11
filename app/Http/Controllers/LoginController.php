a32d7d6d2582b72315d6cf94f9bacfae0c514db0
renanrcrr <renanrcrr.developer@gmail.com>
2025-01-11 18:26:33 +1030

    Validating user and password in DB.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class LoginController extends Controller
 {
@@ -26,6 +27,26 @@ class LoginController extends Controller
         // se não passar pelo validate
         $request->validate($rules, $feedback);
 
-        print_r($request->all());
+        //Recuperamos os parametros do user
+        $email = $request->get('usuario');
+        $password = $request->get('senha');
+
+        echo "Usuário: email | Senha: $password";
+        echo '<br>';
+
+        //Iniciar o Model User
+        $user = new User();
+
+        //Quando a comparacao é por igualdade o operador pode ser omitido
+        $usuario = $user->where('email', $email)
+            ->where('password', $password)
+            ->get()
+            ->first();
+
+        if(isset($usuario->name)){
+            echo 'User exist.';
+        } else {
+            echo 'User does not exist.';
+        }
     }
 }
